ffc8fc0865333426eb340087b9e73ce79709a4a0
Peter Maddison <maddip@nbs.int>
2017-04-27 16:12:32 +0100

    added new classes - emitdemo + viewmodelgeneration

--- Program.cs
+++ Program.cs
@@ -1,15 +1,35 @@
-﻿using System;
-using RosylnCore;
+﻿using RosylynCore;
+using System;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 
-namespace RosylnCore
+namespace RosylynCore
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GenerateSampleViewModel();
+        }
+
+        static void GenerateSampleViewModel()
+        {
+            const string models = @"namespace Models
+{
+    public class Item
+    {
+        public string ItemName { get; set }
+    }
+}
+";
+            var node = CSharpSyntaxTree.ParseText(models).GetRoot();
+            var viewModel = ViewModelGeneration.GenerateViewModel(node);
+
+            if (viewModel != null)
+            {
+                Console.WriteLine(viewModel.ToFullString());
+            }
+            Console.ReadLine();
         }
     }
 }
